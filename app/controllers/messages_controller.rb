d09b1ab277636a931a6fb6ba12cfa51e8a3905e5
pecyanko <psar.xx@icloud.com>
2022-12-26 17:20:11 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
      @message = Message.new
      @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
    end
  
    def create
@@ -10,7 +11,8 @@ class MessagesController < ApplicationController
      if @message.save
        redirect_to room_messages_path(@room)
      else
-       render :index
+      @messages = @room.messages.includes(:user)
+      render :index
      end
    end
  
@@ -20,5 +22,4 @@ class MessagesController < ApplicationController
    def message_params
      params.require(:message).permit(:content).merge(user_id: current_user.id)
    end
- end
- 
+ end                                                  
